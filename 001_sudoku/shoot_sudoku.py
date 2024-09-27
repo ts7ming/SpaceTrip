87df864e595ce452996cbf74eb4ad9334502d7c6
ts7ming <qiming.sci@outlook.com>
2024-09-27 10:41:26 +0800

    update

--- 001_sudoku/shoot_sudoku.py
+++ 001_sudoku/shoot_sudoku.py
@@ -1,39 +1,10 @@
 import sys
-import time
-import random
 import pygame
 
 
-def is_safe(board, row, col, num):
-    # 检查行
-    for x in range(9):
-        if board[row][x] == num:
-            return False
+import sudokum
 
-            # 检查列
-    for x in range(9):
-        if board[x][col] == num:
-            return False
-
-            # 检查3x3的格子
-    start_row = row - row % 3
-    start_col = col - col % 3
-    for i in range(3):
-        for j in range(3):
-            if board[i + start_row][j + start_col] == num:
-                return False
-
-    return True
-
-
-input_sudo = [[0] * 9 for _ in range(9)]
-# 随机填充一些数字以开始解决过程
-for _ in range(80):
-    row = random.randint(0, 8)
-    col = random.randint(0, 8)
-    num = random.randint(1, 9)
-    if is_safe(input_sudo, row, col, num):
-        input_sudo[row][col] = num
+input_sudo = sudokum.generate(mask_rate=0.7)
 
 # input_sudo = [
 #     [0, 9, 7, 3, 6, 0, 4, 8, 5],
@@ -49,6 +20,23 @@ for _ in range(80):
 
 screen_x = 1000
 screen_y = 800
+
+color = {
+    'bg':'Honeydew',
+    'line':'Black',
+    'msg':'Black',
+    'cell_bg': 'Wheat',
+    'cell_font': 'DarkGreen',
+    'selected_cell_bg': 'DarkGreen',
+    'selected_cell_font': 'Wheat',
+    'draft_bg': 'Wheat',
+    'draft_font':'SlateGray',
+    'selected_draft_bg': 'SlateGray',
+    'selected_draft_font':'Wheat',
+}
+
+
+
 
 pygame.init()
 screen = pygame.display.set_mode((screen_x, screen_y))
@@ -74,18 +62,20 @@ def get_vp(xy, value):
 
 
 def shoot(bd, ii, jj, value):
+    """
+    每次调用只做一个步骤
+    """
+    # 清理行: 从每行列块草稿里清除已确定的值
     for n in range(1, 10):
-        # 清理行
         vr = bd[str(ii) + str(n)]
         if len(vr) > 1 and value in vr:
             bd[str(ii) + str(n)] = [x for x in vr if x != value]
             return bd, 'clear value:[ %s ] in cell(%s, %s)' % (str(value), str(ii), str(n)), ii, n
-        # 清理列
+
         vc = bd[str(n) + str(jj)]
         if len(vc) > 1 and value in vc:
             bd[str(n) + str(jj)] = [x for x in vc if x != value]
             return bd, 'clear value:[ %s ] in cell(%s, %s)' % (str(value), str(n), str(jj)), n, jj
-    # 清理块
     for bi in range(int(ii / 3.5) * 3 + 1, int(ii / 3.5) * 3 + 4):
         for bj in range(int(jj / 3.5) * 3 + 1, int(jj / 3.5) * 3 + 4):
             vb = bd[str(bi) + str(bj)]
@@ -93,6 +83,10 @@ def shoot(bd, ii, jj, value):
                 bd[str(bi) + str(bj)] = [x for x in vb if x != value]
                 return bd, 'clear value:[ %s ] in cell(%s, %s)' % (str(value), str(bi), str(bj)), bi, bj
     return 'Done', 'Done', 0, 0
+
+
+def shoot_2():
+    pass
 
 
 def update(bd):
@@ -133,19 +127,29 @@ while True:
     else:
         pass
 
-    screen.fill("black")
+    screen.fill(color['bg'])
+
+    # 画格子
+    g_size = 72
+    g_margin = 8
+    for i in range(1,11):
+        line_width = 5 if i in [1,4,7,10] else 1
+        pygame.draw.line(screen, color['line'], (i*g_size-g_margin, g_size-g_margin), (i*g_size-g_margin, g_size*10-g_margin),line_width)  
+        pygame.draw.line(screen, color['line'], (g_size-g_margin,i*g_size-g_margin), (g_size*10-g_margin,i*g_size-g_margin),line_width)  
+
+    cell_margin = 30
     for i in range(1, 10):
         for j in range(1, 10):
             v = board[str(i) + str(j)]
             if len(v) == 1:
                 cell_radius = 30
                 if i == data[0] and j == data[1]:
-                    font_color = 'white'
-                    cell_color = 'green'
+                    font_color = color['selected_cell_font']
+                    cell_color = color['selected_cell_bg']
                 else:
-                    font_color = 'blue'
-                    cell_color = 'white'
-                cell = (j * 70 + 30, i * 70 + 30)
+                    font_color = color['cell_font']
+                    cell_color = color['cell_bg']
+                cell = (j * g_size + cell_margin, i * g_size + cell_margin)
                 number_font = pygame.font.SysFont("Arial", 20)
                 number_text = number_font.render(str(v[0]), True, font_color)
                 number_rect = number_text.get_rect(center=cell)
@@ -154,13 +158,13 @@ while True:
             else:
                 cell_radius = 10
                 if i == data[2] and j == data[3]:
-                    font_color = 'white'
-                    cell_color = 'green'
+                    font_color = color['selected_draft_font']
+                    cell_color = color['selected_draft_bg']
                 else:
-                    font_color = 'red'
-                    cell_color = 'white'
+                    font_color = color['draft_font']
+                    cell_color = color['draft_bg']
                 for vv in v:
-                    cell = (j * 70 + get_vp('x', vv), i * 70 + get_vp('y', vv))
+                    cell = (j * g_size + get_vp('x', vv), i * g_size + get_vp('y', vv))
                     number_font = pygame.font.SysFont("Arial", 15)
                     number_text = number_font.render(str(vv), True, font_color)
                     number_rect = number_text.get_rect(center=cell)
@@ -173,7 +177,7 @@ while True:
     pi = 80
     for info in info_list:
         font = pygame.font.SysFont("Arial", 16)
-        txt = font.render(info, True, (255, 255, 255))
+        txt = font.render(info, True, color['msg'])
         screen.blit(txt, (800, pi))
         pi += 15
     pygame.display.flip()
