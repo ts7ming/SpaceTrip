30d6dd33ec2f96db00818fd329bc2ade7adb0470
ts7ming <qiming.sci@outlook.com>
2024-09-27 14:34:46 +0800

    update

--- 001_sudoku/shoot_sudoku.py
+++ 001_sudoku/shoot_sudoku.py
@@ -1,25 +1,54 @@
 import sys
 import pygame
-
-
 import sudokum
+from sudoku_lib import Button
 
-input_sudo = sudokum.generate(mask_rate=0.7)
+input_sudo = sudokum.generate(mask_rate=0.4)
+
+input_sudo = [
+    [0, 0, 0, 0, 8, 0, 5, 0, 0],
+    [6, 0, 0, 0, 0, 0, 0, 1, 7],
+    [0, 5, 0, 4, 1, 0, 0, 0, 0],
+    [0, 9, 0, 7, 0, 1, 0, 0, 0],
+    [0, 4, 1, 5, 0, 0, 0, 0, 2],
+    [0, 0, 6, 0, 0, 4, 0, 0, 0],
+    [0, 0, 0, 3, 0, 5, 9, 0, 0],
+    [0, 3, 0, 0, 0, 0, 6, 2, 0],
+    [0, 2, 0, 0, 0, 6, 0, 0, 0]
+]
 
 # input_sudo = [
-#     [0, 9, 7, 3, 6, 0, 4, 8, 5],
-#     [1, 4, 8, 5, 2, 3, 9, 7, 0],
-#     [5, 0, 1, 7, 9, 4, 3, 2, 8],
-#     [6, 8, 9, 0, 7, 2, 5, 3, 0],
-#     [3, 2, 0, 8, 0, 9, 7, 6, 1],
-#     [7, 5, 4, 2, 1, 6, 8, 9, 3],
-#     [9, 7, 6, 4, 8, 0, 2, 1, 3],
-#     [4, 0, 3, 6, 0, 8, 9, 4, 7],
-#     [8, 3, 2, 9, 7, 4, 1, 5, 0]
+#     [3, 5, 7, 0, 4, 2, 1, 0, 0],
+# [1, 0, 0, 5, 0, 0, 0, 0, 7],
+# [8, 0, 4, 0, 7, 1, 2, 0, 3],
+# [6, 3, 0, 7, 0, 4, 8, 2, 1],
+# [9, 0, 0, 2, 0, 8, 0, 0, 4],
+# [2, 4, 0, 1, 5, 6, 7, 0, 0],
+# [4, 6, 9, 8, 2, 0, 3, 1, 0],
+# [5, 0, 3, 0, 1, 9, 6, 0, 2],
+# [7, 0, 2, 0, 6, 0, 0, 0, 8],
 # ]
 
-screen_x = 1000
-screen_y = 800
+for x in input_sudo:
+    print(x,',')
+
+pos = {
+    'board': (50,50),
+    'ctrl': (750, 50),
+    'msg': (850, 50),
+}
+
+size = {
+    'grid': 72,
+    'grid_line': 5,
+    'grid_line_light':1,
+    'grid_margin':6,
+    'cell_margin':30,
+    'cell_radius_cfm':28,
+    'cell_radius_draft':13,
+    'font_cfm':23,
+    'font_draft':15
+}
 
 color = {
     'bg':'Honeydew',
@@ -34,14 +63,6 @@ color = {
     'selected_draft_bg': 'SlateGray',
     'selected_draft_font':'Wheat',
 }
-
-
-
-
-pygame.init()
-screen = pygame.display.set_mode((screen_x, screen_y))
-clock = pygame.time.Clock()
-
 
 def get_vp(xy, value):
     if xy == 'x':
@@ -63,30 +84,85 @@ def get_vp(xy, value):
 
 def shoot(bd, ii, jj, value):
     """
+    从每行列块草稿里清除已确定的值
     每次调用只做一个步骤
     """
-    # 清理行: 从每行列块草稿里清除已确定的值
     for n in range(1, 10):
+        # 行
         vr = bd[str(ii) + str(n)]
         if len(vr) > 1 and value in vr:
             bd[str(ii) + str(n)] = [x for x in vr if x != value]
-            return bd, 'clear value:[ %s ] in cell(%s, %s)' % (str(value), str(ii), str(n)), ii, n
-
+            return 'clear value:[ %s ] in cell(%s, %s)' % (str(value), str(ii), str(n)), ii, n
+        # 列
         vc = bd[str(n) + str(jj)]
         if len(vc) > 1 and value in vc:
             bd[str(n) + str(jj)] = [x for x in vc if x != value]
-            return bd, 'clear value:[ %s ] in cell(%s, %s)' % (str(value), str(n), str(jj)), n, jj
+            return 'clear value:[ %s ] in cell(%s, %s)' % (str(value), str(n), str(jj)), n, jj
+    # 块
     for bi in range(int(ii / 3.5) * 3 + 1, int(ii / 3.5) * 3 + 4):
         for bj in range(int(jj / 3.5) * 3 + 1, int(jj / 3.5) * 3 + 4):
             vb = bd[str(bi) + str(bj)]
             if len(vb) > 1 and value in vb:
                 bd[str(bi) + str(bj)] = [x for x in vb if x != value]
-                return bd, 'clear value:[ %s ] in cell(%s, %s)' % (str(value), str(bi), str(bj)), bi, bj
-    return 'Done', 'Done', 0, 0
+                return 'clear value:[ %s ] in cell(%s, %s)' % (str(value), str(bi), str(bj)), bi, bj
+    return 'Done', 0, 0
 
 
-def shoot_2():
-    pass
+def search(bd):
+    """
+    从每行列块草稿里找到只出现一次的值, 更新为确定值
+    每次调用只做一个步骤
+    """
+    for n in range(1, 10):
+        # 行
+        cfm_v = []
+        for ii in range(1,10): # 遍历cell
+            bdv = bd[str(n)+str(ii)]
+            if len(bdv)==1:
+                cfm_v.append(bdv[0])
+        v_cnt = {}
+        v_cnt_inx = {}
+        for v in range(1,10):
+            if v in cfm_v: continue
+            if v not in v_cnt.keys():
+                v_cnt[v] = 0
+                v_cnt_inx[v]=0
+            for ii in range(1,10):
+                bdv = bd[str(n)+str(ii)]
+                if v in bdv:
+                    v_cnt[v]+=1
+                    v_cnt_inx[v] = ii
+        for v in range(1, 10):
+            if v in cfm_v: continue
+            if v_cnt[v] == 1:
+                bd[str(n)+str(v_cnt_inx[v])] = [v]
+                return 'Rise value:[ %s ] in cell(%s, %s)' % (str(v), str(n), str(v_cnt_inx[v])), n, v_cnt_inx[v]
+        # 列
+        cfm_v = []
+        for ii in range(1,10):
+            bdv = bd[str(ii)+str(n)]
+            if len(bdv)==1:
+                cfm_v.append(bdv[0])
+        v_cnt = {}
+        v_cnt_inx = {}
+        for v in range(1,10):
+            if v in cfm_v: continue
+            if v not in v_cnt.keys():
+                v_cnt[v] = 0
+                v_cnt_inx[v]=0
+            for ii in range(1,10):
+                bdv = bd[str(ii)+str(n)]
+                if v in bdv:
+                    v_cnt[v]+=1
+                    v_cnt_inx[v] = ii
+        for v in range(1, 10):
+            if v in cfm_v: continue
+            if v_cnt[v] == 1:
+                bd[str(v_cnt_inx[v])+str(n)] = [v]
+                return 'Rise value:[ %s ] in cell(%s, %s)' % (str(v), str(n), str(v_cnt_inx[v])), n, v_cnt_inx[v]
+            
+
+    return 'Done', 0, 0
 
 
 def update(bd):
@@ -94,12 +170,30 @@ def update(bd):
         for jj in range(1, 10):
             values = bd[str(ii) + str(jj)]
             if len(values) == 1:
-                status, shoot_msg, rn, cn = shoot(bd, ii, jj, values[0])
+                shoot_msg, rn, cn = shoot(bd, ii, jj, values[0])
                 if shoot_msg == 'Done':
                     continue
                 data = [ii, jj, rn, cn]
-                return bd, shoot_msg, data
-    return bd, 'Done', [0, 0, 0, 0]
+                return shoot_msg, data
+
+    search_msg, rn, cn = search(bd)
+    if search_msg == 'Done':
+        return 'Done', [0, 0, 0, 0]
+    else:
+        data = [0, 0, rn, cn]
+        return search_msg, data
+
+
+screen_x = 1200
+screen_y = 800
+
+
+pygame.init()
+screen = pygame.display.set_mode((screen_x, screen_y))
+clock = pygame.time.Clock()
+
+button_go = Button(750,50,80,50,'Go','SteelBlue')
+button_draft = Button(750,110,80,50,'AutoGo','SteelBlue')
 
 
 board = {}
@@ -112,37 +206,52 @@ for i in range(1, 10):
 count = 0
 msg = ''
 data = [0, 0, 0, 0]
+msg_display = []
+auto = False
+todo = True
 while True:
+    go = False
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-    if pygame.key.get_pressed()[pygame.K_ESCAPE]:
-        sys.exit()
-    elif pygame.mouse.get_pressed() and msg != 'Done':
-        ml, mm, mr = pygame.mouse.get_pressed()
-        px, py = pygame.mouse.get_pos()
-        if ml:
-            count += 1
-            board, msg, data = update(board)
-    else:
-        pass
+        elif button_draft.is_clicked(event):
+            auto = True if auto is False else False
+            button_draft.color = 'Lime' if button_draft.color=='SteelBlue' else 'SteelBlue'
+            break
+        elif button_go.is_clicked(event):
+            go = True
+            button_go.color = 'Lime'
+            break
+        else:
+            pass
+
+    if todo and (auto or go):
+        msg, data = update(board)
+        count += 1
+        if msg not in msg_display:
+            msg_display.append(msg)
+            if len(msg_display)>40:
+                msg_display.pop(0)
+        if msg == 'Done':
+            todo = False
 
     screen.fill(color['bg'])
+    button_go.draw(screen)
+    button_draft.draw(screen)
 
     # 画格子
-    g_size = 72
-    g_margin = 8
+    g_size = size['grid']
+    g_margin = size['grid_margin']
     for i in range(1,11):
-        line_width = 5 if i in [1,4,7,10] else 1
+        line_width = size['grid_line'] if i in [1,4,7,10] else size['grid_line_light']
         pygame.draw.line(screen, color['line'], (i*g_size-g_margin, g_size-g_margin), (i*g_size-g_margin, g_size*10-g_margin),line_width)  
         pygame.draw.line(screen, color['line'], (g_size-g_margin,i*g_size-g_margin), (g_size*10-g_margin,i*g_size-g_margin),line_width)  
 
-    cell_margin = 30
+    cell_margin = size['cell_margin']
     for i in range(1, 10):
         for j in range(1, 10):
             v = board[str(i) + str(j)]
             if len(v) == 1:
-                cell_radius = 30
                 if i == data[0] and j == data[1]:
                     font_color = color['selected_cell_font']
                     cell_color = color['selected_cell_bg']
@@ -150,13 +259,12 @@ while True:
                     font_color = color['cell_font']
                     cell_color = color['cell_bg']
                 cell = (j * g_size + cell_margin, i * g_size + cell_margin)
-                number_font = pygame.font.SysFont("Arial", 20)
+                number_font = pygame.font.SysFont("Arial", size['font_cfm'])
                 number_text = number_font.render(str(v[0]), True, font_color)
                 number_rect = number_text.get_rect(center=cell)
-                pygame.draw.circle(screen, cell_color, cell, cell_radius)
+                pygame.draw.circle(screen, cell_color, cell, size['cell_radius_cfm'])
                 screen.blit(number_text, number_rect)  # 在指定位置绘制文字
             else:
-                cell_radius = 10
                 if i == data[2] and j == data[3]:
                     font_color = color['selected_draft_font']
                     cell_color = color['selected_draft_bg']
@@ -165,20 +273,20 @@ while True:
                     cell_color = color['draft_bg']
                 for vv in v:
                     cell = (j * g_size + get_vp('x', vv), i * g_size + get_vp('y', vv))
-                    number_font = pygame.font.SysFont("Arial", 15)
+                    number_font = pygame.font.SysFont("Arial", size['font_draft'])
                     number_text = number_font.render(str(vv), True, font_color)
                     number_rect = number_text.get_rect(center=cell)
-                    pygame.draw.circle(screen, cell_color, cell, cell_radius)
+                    pygame.draw.circle(screen, cell_color, cell, size['cell_radius_draft'])
                     screen.blit(number_text, number_rect)  # 在指定位置绘制文字
 
     # 概况信息
-
-    info_list = ['step:  ' + str(count), '', msg]
-    pi = 80
+    info_list = ['step:  ' + str(count), ''] + msg_display
+    pi = pos['msg'][1]
     for info in info_list:
-        font = pygame.font.SysFont("Arial", 16)
+        font = pygame.font.SysFont("Arial", size['cell_radius_draft'])
         txt = font.render(info, True, color['msg'])
-        screen.blit(txt, (800, pi))
+        screen.blit(txt, (pos['msg'][0], pi))
         pi += 15
     pygame.display.flip()
     dt = clock.tick(30)
+    button_go.color = 'SteelBlue'
---
+++ 001_sudoku/sudoku_lib.py
@@ -0,0 +1,21 @@
+import pygame
+
+class Button:  
+    def __init__(self, x, y, width, height, text, color=(255, 255, 255)):  
+        self.rect = pygame.Rect(x, y, width, height)  
+        self.color = color  
+        self.text = text  
+        self.font = pygame.font.Font(None, 16)  
+        self.text_surface = self.font.render(self.text, True, (0, 0, 0))  
+  
+    def draw(self, screen):  
+        pygame.draw.rect(screen, self.color, self.rect)  
+        screen.blit(self.text_surface, (self.rect.left + (self.rect.width - self.text_surface.get_width()) // 2,  
+                                        self.rect.top + (self.rect.height - self.text_surface.get_height()) // 2))  
+  
+    def is_clicked(self, event):  
+        if event.type == pygame.MOUSEBUTTONDOWN:  
+            mouse_pos = event.pos  
+            if self.rect.collidepoint(mouse_pos):  
+                return True  
+        return False
